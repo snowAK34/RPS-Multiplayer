58058405c19b0deeea96f29cf13a1197b2c6de88
Stacy Nowak <nowak.stacy@gmail.com>
2019-03-13 19:05:25 -0700

    initial file setup

---
+++ assets/game.js
@@ -0,0 +1,18 @@
+"use strict"
+document.ready(function() {
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAyGSMfQV8ocZUAuWjtoom7T4UbSsLjDNI",
+    authDomain: "rps-multiplayer-387ef.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-387ef.firebaseio.com",
+    projectId: "rps-multiplayer-387ef",
+    storageBucket: "rps-multiplayer-387ef.appspot.com",
+    messagingSenderId: "710419196274"
+  };
+
+  firebase.initializeApp(config);
+
+
+
+});
